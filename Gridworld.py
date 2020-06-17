0fd2e625295abb684b47ec2686caaf86ab785de6
Anirudh Rajiv Menon <36445587+axe76@users.noreply.github.com>
2020-06-17 22:24:27 +0530

    Update Gridworld.py

--- Gridworld.py
+++ Gridworld.py
@@ -30,10 +30,8 @@ class Grid:
         return s not in self.actions
     
     def get_next_state(self, s, a):
-    # this answers: where would I end up if I perform action 'a' in state 's'?
         i, j = s[0], s[1]
     
-        # if this action moves you somewhere else, then it will be in this dictionary
         if a in self.actions[(i, j)]:
           if a == 'U':
             i -= 1
@@ -46,7 +44,6 @@ class Grid:
         return i, j
 
     def move(self, action):
-    # check if legal move first
         if action in self.actions[(self.i, self.j)]:
           if action == 'U':
             self.i -= 1
@@ -56,11 +53,10 @@ class Grid:
             self.j += 1
           elif action == 'L':
             self.j -= 1
-        # return a reward (if any)
+
         return self.rewards.get((self.i, self.j), 0)
 
     def undo_move(self, action):
-    # these are the opposite of what U/D/L/R should normally do
         if action == 'U':
           self.i += 1
         elif action == 'D':
@@ -69,29 +65,18 @@ class Grid:
           self.j -= 1
         elif action == 'L':
           self.j += 1
-        # raise an exception if we arrive somewhere we shouldn't be
-        # should never happen
+        
         assert(self.current_state() in self.all_states())
 
     def game_over(self):
-        # returns true if game is over, else false
-        # true if we are in a state where no actions are possible
         return (self.i, self.j) not in self.actions
 
     def all_states(self):
-        # possibly buggy but simple way to get all states
-        # either a position that has possible next actions
-        # or a position that yields a reward
         return set(self.actions.keys()) | set(self.rewards.keys())
 
 
 def standard_grid():
-  # define a grid that describes the reward for arriving at each state
-  # and possible actions at each state
-  # the grid looks like this
-  # x means you can't go there
-  # s means start position
-  # number means reward at that state
+  #Grid is:
   # .  .  .  1
   # .  x  . -1
   # s  .  .  .
@@ -113,8 +98,6 @@ def standard_grid():
 
 
 def negative_grid(step_cost=-0.1):
-  # in this game we want to try to minimize the number of moves
-  # so we will penalize every move
   g = standard_grid()
   g.rewards.update({
     (0, 0): step_cost,
